163a2ceb2b319a1fc002d46d4ed55ece5f4dd4e8
Rafael Santos <rafael.snv@gmail.com>
2025-05-30 03:57:48 -0300

    New: repositories

---
+++ BlocoNaRua.Data/Repositories/Base/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using BlocoNaRua.Core.Models;
+using BlocoNaRua.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AulaRepositoryPattern.Data.Repositories
+{
+    public class RepositoryBase<TEntity>(AppDbContext appContext) : IRepositoryBase<TEntity> where TEntity : EntityBase
+    {
+        public readonly DbSet<TEntity> _DbSet = appContext.Set<TEntity>();
+        public readonly AppDbContext _AppDbContext = appContext;
+
+        public async Task<IEnumerable<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            var query = _DbSet.AsQueryable();
+
+            if (filter != null)
+                query = query
+                    .Where(filter)
+                    .AsNoTracking();
+
+            return await query.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _DbSet.FindAsync(id);
+        }
+
+        public async Task<int> AddAsync(TEntity entity)
+        {
+            await _DbSet.AddAsync(entity);
+            return await _AppDbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            _DbSet.Remove(entity);
+            await _AppDbContext.SaveChangesAsync();
+        }
+
+        public async Task<int> UpdateAsync(TEntity entity)
+        {
+            _DbSet.Update(entity);
+            return await _AppDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ BlocoNaRua.Data/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+﻿using AulaRepositoryPattern.Data.Repositories;
+using BlocoNaRua.Data.Context;
+using BlocoNaRua.Data.Repositories.Interfaces;
+using BlocoNaRua.Domain.Entities;
+
+namespace BlocoNaRua.Data.Repositories;
+
+public class UserRepository(AppDbContext appContext) : RepositoryBase<UserEntity>(appContext), IUserRepository
+{
+}
