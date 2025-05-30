08880fe427f584b107a54c01c78f74a6d5cb8eeb
Rafael Santos <rafael.snv@gmail.com>
2025-05-30 03:57:30 -0300

    New: meeting presence fluent api mapping

---
+++ BlocoNaRua.Data/Mapping/MeetingPresenceConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BlocoNaRua.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlocoNaRua.Data.Mapping;
+
+public class MeetingPresenceConfiguration : IEntityTypeConfiguration<MeetingPresenceEntity>
+{
+    public void Configure(EntityTypeBuilder<MeetingPresenceEntity> builder)
+    {
+        builder.Property(e => e.IsPresent)
+               .IsRequired();
+
+        builder.HasIndex(e => new { e.MeetingId, e.UserId })
+               .IsUnique();
+    }
+}
