674846ed3d5446bc88bc2ebf4ff9f87f496825fc
Rafael Santos <rafael.snv@gmail.com>
2025-05-30 03:58:02 -0300

    New: context and extensions

---
+++ BlocoNaRua.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BlocoNaRua.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlocoNaRua.Data.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<CarnivalBlockEntity> CarnivalBlocks { get; set; }
+    public DbSet<MeetingEntity> Meetings { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<CarnivalBlockUserEntity> CarnivalBlockUsers { get; set; }
+    public DbSet<MeetingPresenceEntity> MeetingAttendances { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // CarnivalBlock
+    }
+}
---
+++ BlocoNaRua.Data/Extensions/DataExtensions.cs
@@ -0,0 +1,31 @@
+﻿using AulaRepositoryPattern.Data.Repositories;
+using BlocoNaRua.Core.Models;
+using BlocoNaRua.Data.Context;
+using BlocoNaRua.Data.Repositories;
+using BlocoNaRua.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlocoNaRua.Data.Extensions;
+
+public static class DataExtensions
+{
+    public static IServiceCollection AddEntityFramework(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+        services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
+
+}
